51d675eca767df42c96b1e69a00570fcd4d6c170
SamiAlasfahani <dindle21@gmail.com>
2022-02-05 20:24:31 -0500

    improved documentation for ElevatorData

--- elevator-project/src/main/java/elevatorstuff/ElevatorData.java
+++ elevator-project/src/main/java/elevatorstuff/ElevatorData.java
@@ -21,9 +21,13 @@ public class ElevatorData {
 	 * constructor for ElevatorData, fills all fields
 	 * 
 	 * @param _requestDirection
+	 * this is the direction that the elevator is requested to go in
 	 * @param _sourceFloor
+	 * this is the floor that a request came from
 	 * @param _destFloor
+	 * this is the destination floor of the request
 	 * @param _time
+	 * this is the time the request was made at
 	 */
 	public ElevatorData( boolean _requestDirection, Integer _sourceFloor, Integer _destFloor, LocalTime _time) {
 		requestDirection = _requestDirection;
@@ -52,6 +56,7 @@ public class ElevatorData {
 	 * returns the floor that a request was sourced from, can return null if the request was from an elevator
 	 * 
 	 * @return sourceFloor
+	 * the floor the request came from
 	 */
 	public Integer getSourceFloor() {
 		return sourceFloor;
@@ -61,6 +66,7 @@ public class ElevatorData {
 	 * returns the floor that a request is destined for, can return null if the request is calling an elevator car
 	 * 
 	 * @return destFloor
+	 * the floor the request is sending the elevator to
 	 */
 	public Integer getDestFloor() {
 		return destFloor;
@@ -70,6 +76,7 @@ public class ElevatorData {
 	 * returns the time a request was made at
 	 * 
 	 * @return time
+	 * the time the request was made
 	 */
 	public LocalTime getTime() {
 		return time;
